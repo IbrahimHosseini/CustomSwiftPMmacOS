ae7bcff3181eb4371a888cb7ecc3662a01366eda
Ibrahim Hosseini <sehosseini@me.com>
2024-03-09 18:21:35 +0330

    Initial Commit

---
+++ sampleView/sampleViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleViewApp.swift
+//  sampleView
+//
+//  Created by Ibrahim on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct sampleViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
